58da15205e11850258ffdbf4b1d9521181a93748
Matthew Akahomen <alexislordqtest@gmail.com>
2025-01-03 23:46:28 +0100

    Very minor UI update

---
+++ frontend/app/landing/feature/page.jsx
@@ -0,0 +1,10 @@
+"use client"
+
+
+export default function Feature() {
+    return (
+        <main>
+        <h1 className="text-xl lg:text-4xl text-center font-semibold">What is Clyne?👀 </h1>
+        </main>
+    )
+}
--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import NavBar from "./components/nav";
 import Hero from "./components/hero";
+import Feature from "./landing/feature/page";
 
 export default function Home() {
   return (
     <main>
       <NavBar />
-     <Hero />
+      <Hero />
+      <Feature />
     </main>
   );
 }
