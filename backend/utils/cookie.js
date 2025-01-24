c43cd471bd131b15f76267453a0af682250f55eb
Matthew Akahomen <alexislordqtest@gmail.com>
2025-01-24 22:21:08 +0100

    :(

--- backend/utils/cookie.js
+++ backend/utils/cookie.js
@@ -1,9 +1,13 @@
+import { frontendUrl } from "./urls.js";
+
 export const setCookie = (res, token, expiresIn) => {
   const cookieOptions = {
     maxAge: expiresIn,
     httpOnly: true,
     secure: process.env.NODE_ENV === "production",
     sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    domain: frontendUrl,
+    path: "/"
   };
   res.cookie("token", token, cookieOptions);
 };
